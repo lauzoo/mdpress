a00594844b43d579d85e771d29c97fd7c9112c35
Vasquez <liqianglau@outlook.com>
2015-02-21 01:34:10 +0800

    增加提取文本标题

--- save.php
+++ save.php
@@ -1,4 +1,6 @@
 <?php
+include 'lib/Parser.php';
+use HtmlParser\Parser;
 
 $name = $_REQUEST["name"];
 $content = $_REQUEST["content"];
@@ -17,6 +19,18 @@ if ($conn->connect_error)
     die ("Connection Failed: " . $conn->connect_error);
 }
 echo "Connection Success<br/>";
+
+
+
+//现在支持直接输入html字符串了
+$html_dom = new Parser($content);
+echo "1";
+//$html_dom->parseStr($html);
+$div = $html_dom->find('h2',0);
+echo "2";
+$name = $div->getPlainText();
+echo "3" . "<br/>";
+echo $name . "<br/>";
 
 $sql = "INSERT INTO posts(name, content) VALUES('" . $name . "', '" . $content . "')";
 echo $sql . "<br/>";
