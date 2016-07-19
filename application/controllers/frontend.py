7c9a2d40efbba3686ced16adc8c1ff1c30c170bd
liqiang <liqiang.liu@imaygou.com>
2016-07-20 00:31:46 +0800

    fix: make archive

--- application/controllers/frontend.py
+++ application/controllers/frontend.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 import os
+from functools import partial
 
 from flask import (request, Blueprint, render_template,
                    send_from_directory, current_app as app)
@@ -44,20 +45,47 @@ def index():
 
 @frontend_bp.route('/archive')
 def archive():
-    env = {
+    base_env = {
         'site': {
             'title': 'Hello'
         },
         'has': lambda x: False,
         'paginator': {
-            'has_pre': True,
-            'has_next': True,
+            'has_pre': False,
+            'has_next': False,
         },
         'pager': {
             'pre_url': '',
             'next_url': ''
         },
     }
+
+    def group(self, *args, **kwargs):
+        return self
+
+    class Object(object):
+        date = {
+            'format': lambda x: x,
+        }
+        metadata = {
+            'refer': 'refer'
+        }
+
+    def get_data(type, sort, limit):
+        print 'type: {}'.format(type)
+        print 'sort: {}'.format(sort)
+        p1 = Object()
+        p1.year='2015'
+        p2 = Object()
+        p2.year='2016'
+        return [{'2015', (p1,)},
+                {'2016', (p2,)}]
+
+    env = {
+        'get_data': get_data
+    }
+    env.update(base_env)
+
     return render_template('archive.jade', **env)
 
 
@@ -69,12 +97,9 @@ def post(post_id):
 @frontend_bp.route('/template/<filename>')
 def template_static(filename):
     template_path = os.path.join(app.config['PROJECT_PATH'], 'application/templates')
-    print filename[-4:] == 'scss'
     if filename[-4:] == 'scss':
         file_path = os.path.join(template_path, filename)
-        filename = filename[-4:] + "css"
-        src = open(file_path).read()
-        print len(src)
+        filename = "{}{}".format(filename[:-4], "css")
         with open(os.path.join(template_path, filename), 'w') as f:
             f.write(Compiler().compile(file_path))
 
