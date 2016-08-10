ee236012d6f5d3060924871df84f35c289fb93e8
tyrael <liqianglau@outlook.com>
2016-08-11 00:32:48 +0800

    feat: backup image to dropbox

--- utils/backup_images.py
+++ utils/backup_images.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python
 # encoding: utf-8
+import urllib
+import dropbox
+from dropbox.files import WriteMode
 from BeautifulSoup import BeautifulSoup
 
 from application.models import Post, Images
+
+
+dbx = dropbox.Dropbox("YOUR_ACCESS_TOKEN")
+
+
+def save(url):
+    filename = url.split('/')[-1]
+    path = r"/tmp/upload/{}".format(filename)
+    try:
+        data = urllib.urlopen(url).read()
+    except Exception:
+        return ""
+    with open(path, "wb") as f:
+        f.write(data)
+    return path
 
 
 def backup():
@@ -15,6 +33,15 @@ def backup():
                     if attr[0] == 'src':
                         url = attr[1]
                         Images(post_id=int(post.id), url=url).save()
+
+    for img in Images.objects.all():
+        path = save(img.url)
+        if path:
+            img.local_path = path
+            img.save()
+            with open(img.local_path, 'rb') as f:
+                image_name = '/' + img.local_path.split('/')[-1]
+                dbx.files_upload(f, image_name, mode=WriteMode('overwrite'))
 
 
 if __name__ == '__main__':
